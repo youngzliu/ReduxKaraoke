8d72ef38d14ef596f33bb9fc49980eb428f2ad45
Young Liu <youngliu92@gmail.com>
2019-05-02 10:11:07 -0700

    add initial parts of redux.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
+// LYRIC INFO
+const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(
+  ", "
+);
+
+// INITIAL REDUX STATE
+const initialState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0
+};
+
+// REDUCER WILL GO HERE
+
+// JEST TESTS + SETUP WILL GO HERE
+
+// REDUX STORE
+const { createStore } = Redux;
+const store = createStore(reducer);
+console.log(store.getState());
+
+// CLICK LISTENER
 const userClick = () => {
-  console.log('click');
-}
+  console.log("click");
+};
