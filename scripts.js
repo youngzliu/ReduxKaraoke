ddb9d9099cda60fa12dfb678cfc9d43d415ee7bb
Young Liu <youngliu92@gmail.com>
2019-05-02 10:54:33 -0700

    add a reducer and some initial tests.

--- scripts.js
+++ scripts.js
@@ -10,8 +10,29 @@ const initialState = {
 };
 
 // REDUCER WILL GO HERE
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "NEXT_LYRIC":
+      let newArrayPosition = state.arrayPosition + 1;
+      let newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition
+      };
+      return newState;
+    default:
+      return state;
+  }
+};
 
 // JEST TESTS + SETUP WILL GO HERE
+const { expect } = window;
+
+expect(reducer(initialState, { type: null })).toEqual(initialState);
+
+expect(reducer(initialState, { type: "NEXT_LYRIC" })).toEqual({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1
+});
 
 // REDUX STORE
 const { createStore } = Redux;
