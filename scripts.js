08cdc047b52528f15165f07c7dbe96e8576f4e24
Young Liu <youngliu92@gmail.com>
2019-05-02 14:22:19 -0700

    add conditional to click to restart song when you get to the end.

--- scripts.js
+++ scripts.js
@@ -11,13 +11,17 @@ const initialState = {
 
 // REDUCER WILL GO HERE
 const reducer = (state = initialState, action) => {
+  let newState;
   switch (action.type) {
     case "NEXT_LYRIC":
       let newArrayPosition = state.arrayPosition + 1;
-      let newState = {
+      newState = {
         songLyricsArray: state.songLyricsArray,
         arrayPosition: newArrayPosition
       };
+      return newState;
+    case "RESTART_SONG":
+      newState = initialState;
       return newState;
     default:
       return state;
@@ -33,6 +37,16 @@ expect(reducer(initialState, { type: "NEXT_LYRIC" })).toEqual({
   songLyricsArray: songLyricsArray,
   arrayPosition: 1
 });
+
+expect(
+  reducer(
+    {
+      songLyricsArray: songLyricsArray,
+      arrayPosition: 1
+    },
+    { type: "RESTART_SONG" }
+  )
+).toEqual(initialState);
 
 // REDUX STORE
 const { createStore } = Redux;
@@ -58,8 +72,12 @@ window.onload = function() {
 
 // CLICK LISTENER
 const userClick = () => {
-  store.dispatch({ type: "NEXT_LYRIC" });
-  console.log(store.getState());
+  const currentState = store.getState();
+  if (currentState.arrayPosition === currentState.songLyricsArray.length - 1) {
+    store.dispatch({ type: "RESTART_SONG" });
+  } else {
+    store.dispatch({ type: "NEXT_LYRIC" });
+  }
 };
 
 // SUBSCRIBE TO REDUX STORE
